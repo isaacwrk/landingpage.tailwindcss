eddaa07e45f2ae805acbddc4adf9ff039fa6837e
isaacwrk <isaacsouzawrk@gmail.com>
2021-11-03 09:11:56 -0300

    slider working

--- src/components/Carrousel.vue
+++ src/components/Carrousel.vue
@@ -1,26 +1,32 @@
 <template>
-    <div class="carousel relative shadow-2xl bg-white">
+    <div class="carousel relative shadow-2xl bg-gray">
         <div class="carousel-inner relative overflow-hidden w-full">
             <!--Slide 1-->
             <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked">
-            <div class="carousel-item absolute opacity-0" style="height:50vh;">
-                <div class="block h-full w-full bg-indigo-500 text-white text-5xl text-center">Slide 1</div>
+            <div class="carousel-item absolute opacity-0" style="height:85vh;">
+                <div class="max-w-full">
+                    <img src="~@/assets/w-mt03.jpg" alt="">
+                </div>
             </div>
             <label for="carousel-3" class="prev control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
             <label for="carousel-2" class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
 		
             <!--Slide 2-->
             <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
-            <div class="carousel-item absolute opacity-0" style="height:50vh;">
-                <div class="block h-full w-full bg-orange-500 text-white text-5xl text-center">Slide 2</div>
+            <div class="carousel-item absolute opacity-0" style="height:85vh;">
+                <div class="max-w-full">
+                    <img src="~@/assets/w-mt07.jpg" alt="">
+                </div>
             </div>
             <label for="carousel-1" class="prev control-2 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
             <label for="carousel-3" class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label> 
 		
             <!--Slide 3-->
             <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
-            <div class="carousel-item absolute opacity-0" style="height:50vh;">
-                <div class="block h-full w-full bg-green-500 text-white text-5xl text-center">Slide 3</div>
+            <div class="carousel-item absolute opacity-0" style="height:85vh;">
+                <div class="max-w-full">
+                    <img src="~@/assets/w-mt09.jpg" alt="">
+                </div>
             </div>
             <label for="carousel-2" class="prev control-3 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
             <label for="carousel-1" class="next control-3 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
@@ -52,6 +58,11 @@ export default Carrousel;
 </script>
 
 <style>
+
+            .w-mt03{
+                background-image:url("~@/assets/w-mt07.jpg");
+                
+            }
 			.carousel-open:checked + .carousel-item {
 				position: static;
 				opacity: 100;
@@ -75,10 +86,11 @@ export default Carrousel;
 				right: 0;
 				text-align: center;
 				z-index: 10;
+                color: white;
 			}
 			#carousel-1:checked ~ .control-1 ~ .carousel-indicators li:nth-child(1) .carousel-bullet,
 			#carousel-2:checked ~ .control-2 ~ .carousel-indicators li:nth-child(2) .carousel-bullet,
 			#carousel-3:checked ~ .control-3 ~ .carousel-indicators li:nth-child(3) .carousel-bullet {
-				color: #2b6cb0;  /*Set to match the Tailwind colour you want the active one to be */
+				color: #fceb00;  /*Set to match the Tailwind colour you want the active one to be */
 			}
 </style>
