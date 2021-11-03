07b72d9af7a152d89bb3dc5192231b10cf405bda
isaacwrk <isaacsouzawrk@gmail.com>
2021-11-03 08:45:00 -0300

    creating slider component

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,14 +1,20 @@
 <template>
     <NavBar/>
+    <div class="mt-1">
+        <Carrousel/>
+    </div>
+    
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
 import NavBar from '@/components/Nav.vue';
+import Carrousel from '@/components/Carrousel.vue';
 
 const landingPage = defineComponent({
     components:{
-        NavBar
+        NavBar,
+        Carrousel
     }
 });
 
